6a5a4a52b4296996044b16b8f5d004fc24764d87
Samuel Talbot <talbosa@whs.school.nz>
2023-05-19 12:36:48 +1200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ * Title: Game
+ * Author:Samuel Talbot
+ * Date:19/05/2022
+ * Version: 0.0
+ * Purpose: AS91883 & 91884 Programming project
+ **/
+
+let ctx;
+
+window.onload = onStartup;
+function onStartup() {
+    ctx = document.getElementById("gameCanvas").getContext("2d");
+}
