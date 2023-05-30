2f1ed905e5a88d9ecac181971f33365b37bc75e2
Samuel Talbot <talbosa@whs.school.nz>
2023-05-31 09:50:39 +1200

    started learning webgl with pixijs

--- powerup.js
+++
@@ -1,7 +0,0 @@
-class PowerUp {
-    constructor() {
-        this.xPos = WIDTH;
-        this.yPos = Math.random() * HEIGHT;
-        this.type = randomWithProbability([0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3]); // 0 = Damage; 1 = Heal 1; 2 = Add 1 Shield; 3 = Add Max Health
-    }
-}
