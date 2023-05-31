d399137a8ce3b863ec71013e4de6a4cd778a9ceb
Samuel Talbot <talbosa@whs.school.nz>
2023-05-31 12:15:43 +1200

    started porting player class

---
+++ player.js
@@ -0,0 +1,23 @@
+class Player{
+    constructor() {
+        this.image = new Image();
+        this.reset();
+    }
+
+    reset() {
+        this.animation = [];
+        this.animationSpeedMS = 0;
+        this.animationInterval;
+        this.animationIndex = 0;
+        this.xPos = 100;
+        this.yPos = 100;
+        this.width = 50;
+        this.height = 128;
+        this.health = 3;
+        this.maxHealth = 3;
+        this.shield = 0;
+        this.hitBox = false;
+        this.moveSpeedX = SCROLLSPEED;
+        this.moveSpeedY = 10;
+    }
+}
